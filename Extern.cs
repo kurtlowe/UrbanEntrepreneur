f0ea3a572da4a6e0a504ece466e54bc8a402513b
dmwhitedragon <david@shadowdevelopment.com>
2025-04-06 13:37:48 +1000

    faster keys

---
+++ Extern.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UrbanEntrepreneur;
+
+internal static class Extern
+{
+    [DllImport("user32.dll")]
+    public static extern short GetAsyncKeyState(int vKey);
+
+    public const int VK_W = 0x57;
+    public const int VK_S = 0x53;
+    public const int VK_A = 0x41;
+    public const int VK_D = 0x44;
+    public const int VK_ESCAPE = 0x1B;
+}
--- Program.cs
+++ Program.cs
@@ -14,27 +14,27 @@ var y = 1;
 while (true)
 {
     map.DisplayMap(x, y);
-    if (Console.KeyAvailable)
+
+    if (Extern.GetAsyncKeyState(Extern.VK_W) < 0)
     {
-        var key = Console.ReadKey(true).Key;
-        switch (key)
-        {
-            case ConsoleKey.W:
-                if (map.CheckMap(x, y - 1)) y--;
-                break;
-            case ConsoleKey.S:
-                if (map.CheckMap(x, y + 1)) y++;
-                break;
-            case ConsoleKey.A:
-                if (map.CheckMap(x - 1, y)) x--;
-                break;
-            case ConsoleKey.D:
-                if (map.CheckMap(x + 1, y)) x++;
-                break;
-            case ConsoleKey.Escape:
-                Environment.Exit(0);
-                break;
-        }
-        while (Console.KeyAvailable) { Console.ReadKey(true); }
+        if (map.CheckMap(x, y - 1)) y--;
     }
+    if (Extern.GetAsyncKeyState(Extern.VK_S) < 0)
+    {
+        if (map.CheckMap(x, y + 1)) y++;
+    }
+    if (Extern.GetAsyncKeyState(Extern.VK_A) < 0)
+    {
+        if (map.CheckMap(x - 1, y)) x--;
+    }
+    if (Extern.GetAsyncKeyState(Extern.VK_D) < 0)
+    {
+        if (map.CheckMap(x + 1, y)) x++;
+    }
+    if (Extern.GetAsyncKeyState(Extern.VK_ESCAPE) < 0)
+    {
+        Environment.Exit(0);
+    }
+
+    Thread.Sleep(5);
 }
