a683c29a4c0b5a2cff0649331474cbec2b795d86
dmwhitedragon <david@shadowdevelopment.com>
2025-04-06 13:19:41 +1000

    map 4 fun

---
+++ Map.cs
@@ -0,0 +1,86 @@
+﻿namespace UrbanEntrepreneur;
+
+internal class Map
+{
+    private readonly int _windowSize = 10;
+    private readonly List<string> _data = [
+        "╔═════════════════════════════════╗",
+        "║  ^                              ║",
+        "║      ╔═D═╗           ╔═D═╗      ║",
+        "║      ║   ║           ║   ║      ║",
+        "║      ╚═══╝    ^      ╚═══╝      ║",
+        "║                             ^   ║",
+        "║  ^                              ║",
+        "║      ╔══════════ ══════════╗    ║",
+        "║      ║               ^     ║    ║",
+        "║      ║  ~~~~~~~~~~~~~~~~   ║    ║",
+        "║      ║  ~~~~~~~~~~~~~~~~   ║    ║",
+        "║      ║  ~~~~~~~~~~~~~~~~   ║    ║",
+        "║      ║    ^                ║    ║",
+        "║      ╚══════════ ══════════╝    ║",
+        "║                                 ║",
+        "║               ^            ^    ║",
+        "║      ╔═D═╗           ╔═D═╗      ║",
+        "║      ║   ║           ║   ║      ║",
+        "║      ╚═══╝   ^       ╚═══╝      ║",
+        "║                                 ║",
+        "╚═════════════════════════════════╝",
+    ];
+
+    public bool CheckMap(int x, int y)
+    {
+        if (x < 0 || x >= _data[0].Length || y < 0 || y >= _data.Count)
+        {
+            return false;
+        }
+        var c = _data[y][x];
+        return c == ' ' || c == '~';
+    }
+
+    public void DisplayMap(int centerX, int centerY)
+    {
+        Console.SetCursorPosition(0, 0);
+        var halfWindowSize = _windowSize / 2;
+        var startX = centerX - halfWindowSize;
+        var endX = centerX + halfWindowSize;
+        var startY = centerY - halfWindowSize;
+        var endY = centerY + halfWindowSize;
+
+        for (var y = startY; y <= endY; y++)
+        {
+            for (var x = startX; x <= endX; x++)
+            {
+                if (x < 0 || x >= _data[0].Length || y < 0 || y >= _data.Count)
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(' ');
+                }
+                else
+                {
+                    var c = _data[y][x];
+                    if (c == '╦' || c == '╩' || c == '╔' || c == '╗' || c == '╚' || c == '╝' || c == '═' || c == '║')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else if (c == '~') { Console.ForegroundColor = ConsoleColor.Blue; }
+                    else if (c == '^') { Console.ForegroundColor = ConsoleColor.DarkGreen; }
+                    else if (c == 'D') { Console.ForegroundColor = ConsoleColor.Gray; }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                    if (centerX == x && centerY == y)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('@');
+                    }
+                    else
+                    {
+                        Console.Write(c);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,3 +6,31 @@ Console.CursorVisible = false;
 var textGenerator = new GameText();
 textGenerator.WelcomeMessage();
 textGenerator.PlayerInfoMenu();
+
+Console.Clear();
+var map = new Map();
+var x = 1;
+var y = 1;
+while (true)
+{
+    map.DisplayMap(x, y);
+    var key = Console.ReadKey(true).Key;
+    switch (key)
+    {
+        case ConsoleKey.W:
+            if (map.CheckMap(x, y - 1)) y--;
+            break;
+        case ConsoleKey.S:
+            if (map.CheckMap(x, y + 1)) y++;
+            break;
+        case ConsoleKey.A:
+            if (map.CheckMap(x - 1, y)) x--;
+            break;
+        case ConsoleKey.D:
+            if (map.CheckMap(x + 1, y)) x++;
+            break;
+        case ConsoleKey.Escape:
+            Environment.Exit(0);
+            break;
+    }
+}
