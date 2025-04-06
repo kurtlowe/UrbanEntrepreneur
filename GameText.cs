aa1c0bc6e7b7abbadc9f023ebe1ed4680e7db28a
kurtlowe <109322805+kurtlowe@users.noreply.github.com>
2025-04-06 12:17:56 +1000

    Centered Welcome Message

--- GameText.cs
+++ GameText.cs
@@ -7,11 +7,7 @@ internal class GameText
         string weedLeaf = "                        .\r\n                       .:.\r\n                       :|:\r\n                      .:|:.\r\n                      ::|::\r\n       :.             ::|::             .:\r\n       :|:.          .::|::.          .:|:\r\n       ::|:.         :::|:::         .:|:;\r\n       `::|:.        :::|:::        .:|::'\r\n        ::|::.       :::|:::       .::|:;\r\n        `::|::.      :::|:::      .::|::'\r\n         :::|::.     :::|:::     .::|::;\r\n         `:::|::.    :::|:::    .::|::;'\r\n`::.      `:::|::.   :::|:::   .::|::;'      .:;'\r\n `:::..     ¹::|::.  :::|:::  .::|::¹    ..::;'\r\n   `:::::.    ':|::. :::|::: .::|:'   ,::::;'\r\n     `:::::.    ':|:::::|:::::|:'   :::::;'\r\n       `:::::.:::::|::::|::::|::::.,:::;'\r\n          ':::::::::|:::|:::|:::::::;:'\r\n             ':::::::|::|::|:::::::''\r\n                  `::::::::::;'\r\n                 .:;'' ::: ``::.\r\n                      :':':\r\n                        ;";
         string welcomeLogo = " _   _      _                                                  \r\n| | | |_ __| |__   __ _ _ __                                   \r\n| | | | '__| '_ \\ / _` | '_ \\                                  \r\n| |_| | |  | |_) | (_| | | | |                                 \r\n \\___/|_|  |_.__/ \\__,_|_| |_|                                 \r\n _____       _                                                 \r\n| ____|_ __ | |_ _ __ ___ _ __  _ __ ___ _ __   ___ _   _ _ __ \r\n|  _| | '_ \\| __| '__/ _ | '_ \\| '__/ _ | '_ \\ / _ | | | | '__|\r\n| |___| | | | |_| | |  __| |_) | | |  __| | | |  __| |_| | |   \r\n|_____|_| |_|\\__|_|  \\___| .__/|_|  \\___|_| |_|\\___|\\__,_|_|   \r\n                         |_|                                   ";
         Console.ForegroundColor = ConsoleColor.Green;
-        CenterText(weedLeaf);
-        /*Console.WriteLine(weedLeaf);
-        Console.ResetColor();
-        Console.WriteLine(welcomeLogo);
-        Console.WriteLine("Press Enter To Continue....");*/
+        CenterText(weedLeaf, welcomeLogo);
     }
 
     public void PlayerInfoMenu() 
@@ -19,17 +15,19 @@ internal class GameText
          
     }
 
-    public void CenterText(string weedLeaf)
+    public void CenterText(string weedLeaf, string welcomeLogo)
     {
         int windowWidth = Console.WindowWidth;
         int textLength = 50;
         int leftPadding = (windowWidth - textLength) / 2;
-        if (leftPadding < 0) leftPadding = 0; // Avoid negative padding
-
-        Console.Write("hi\r\n");
-        Console.WriteLine("hi");
+        if (leftPadding < 0) leftPadding = 0;
 
         foreach (var line in weedLeaf.Split("\r\n", StringSplitOptions.None))
+        {
+            Console.WriteLine(new string(' ', leftPadding) + line);
+        }
+
+        foreach (var line in welcomeLogo.Split("\r\n", StringSplitOptions.None))
         {
             Console.WriteLine(new string(' ', leftPadding) + line);
         }
